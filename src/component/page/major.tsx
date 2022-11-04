eef04c9846bf56d85793fa9c78af4bddfca39703
Mohamed keita <keitamohamed12@gmail.com>
2022-11-04 13:26:16 -0400

    Major section

--- src/component/page/major.tsx
+++ src/component/page/major.tsx
@@ -1,7 +1,20 @@
+import {useContext, useEffect} from "react";
+
 import {useAppStore} from "../../setup/academy/useReduxHook";
+import {DashboardContext} from "../../setup/context/Context";
+import {accordion} from "../../util/accordion";
 
 export const Major = () => {
+    const dashCtx = useContext(DashboardContext)
     const {major} = useAppStore((state) => state.academics)
+
+    const setActionType = () => {
+        dashCtx.setActionType('')
+    }
+
+    useEffect(() => {
+        accordion()
+    }, [])
 
     return(
         <div className='major'>
@@ -12,10 +25,20 @@ export const Major = () => {
                 <div className="sidenav col-span-4">
                     <h2>This is a side nav</h2>
                 </div>
-                <div className="main col-span-8">
-                    <div className="accordion">
-                        <li className="btn">Overview</li>
-                        <li className="btn">Requirements</li>
+                <div className="accordion col-span-8">
+                    <div className="accordion-tabs !grid-cols-2">
+                        <li className="accordion-tab" data-actab-id='1' onClick={setActionType}>Overview</li>
+                        <li className="accordion-tab" data-actab-id='2' onClick={setActionType}>Requirements</li>
+                    </div>
+                    <div className="accordion-content">
+                        <section className="tab-section" data-actab-id='1'>
+                            <div className="description">
+                                {major?.description}
+                            </div>
+                        </section>
+                        <section className="tab-section" data-actab-id='2'>
+                            <h1>Tab 2</h1>
+                        </section>
                     </div>
                 </div>
             </div>
